2efa208b69c37398d54a33626cfc19a4e099bd71
Watthesmile <118444787+Watthesmile@users.noreply.github.com>
2023-01-19 14:34:44 +0300

    Update new.py

--- new.py
+++ new.py
@@ -4,7 +4,7 @@ def daorne(filePath):
             return True
     except FileNotFoundError as e:
         return False
-if daorne(max) == False:
-    p = open('max.txt','w')
+if daorne(maxx) == False:
+    p = open('maxx.txt','w')
     p.write('0')
     p.close()
